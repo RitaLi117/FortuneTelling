b5c2c4a49b3ef9e25431d7fd2c3505dcf7df8097
李采縈 <leetsaiying@32-252.wireless-city.scu.edu.tw>
2018-07-26 10:10:57 +0800

    Initial Commit

---
+++ FortuneTelling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FortuneTelling
+//
+//  Created by 李采縈 on 2018/7/26.
+//  Copyright © 2018年 李采縈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
